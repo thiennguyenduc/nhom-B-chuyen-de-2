9351804bd8ca710858812c45df8db173da370098
Hoaisme <hoaisme1995@gmail.com>
2017-08-22 19:36:21 +0700

    NguyenDuyHoa
    API dao dien nguoi benh

--- Code/NguyenDuyHoa/gen/com/androidbegin/filterlistviewtutorial/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.androidbegin.filterlistviewtutorial;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
